777c18f00a6b7448780b8e8ea941cfc35481175f
Lisonallie <allisonvanlinden@gmail.com>
2019-06-17 10:30:31 +0200

    add const for js

---
+++ myscript.js
@@ -0,0 +1,6 @@
+const result = document.getElementById("result");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const computer = document.getElementById("computer");
+const reset = document.getElementById("reset");
