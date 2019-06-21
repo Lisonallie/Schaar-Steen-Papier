b8fbb248a9a4f4466181f467b241cb088d015d8b
Lisonallie <allisonvanlinden@gmail.com>
2019-06-21 17:17:19 +0200

    remove computer function and adjust

--- myscript.js
+++ myscript.js
@@ -2,7 +2,6 @@ const result = document.getElementById("result");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
-const computer = document.getElementById("computer");
 const reset = document.getElementById("reset");
 
 function convertToWord(word) {
